9d901958360ad80f82ce621c30fd5b111c71c405
LudySK8 <148266934+LudySK8@users.noreply.github.com>
2024-01-31 18:49:15 -0300

    Update Cartório.c

--- Cartório.c
+++ Cartório.c
@@ -59,7 +59,7 @@ int registro()
 
 int consulta()
 {
-    setlocale(LC_ALL, "PortuguÍs"); //Definindo vari·veis
+    setlocale(LC_ALL, "Portugu√™s"); //Definindo vari√°veis
 
     char cpf[40];
     char conteudo[200]; 
@@ -72,12 +72,12 @@ int consulta()
 
     if (file == NULL)
     {
-        printf("N„o foi possivel abrir o arquivo, n„o localizado!.\n");
+        printf("N√£o foi possivel abrir o arquivo, n√£o localizado!.\n");
     }
 
     while (fgets(conteudo, 200, file) != NULL)
     {
-        printf("\nEssas s„o as informaÁıes do usu·rio: ");
+        printf("\nEssas s√£o as informa√ß√µes do usu√°rio: ");
         printf("%s", conteudo);
         printf("\n\n"); 
     }
@@ -89,7 +89,7 @@ int deletar()
 {
     char cpf[40];
     
-    printf("Digite o CPF do usu·rio a ser deletado: ");
+    printf("Digite o CPF do usu√°rio a ser deletado: ");
     scanf("%s", cpf);
     
     remove(cpf);
@@ -99,29 +99,30 @@ int deletar()
     
     if(file == NULL)
     {
-    	printf("O usu·rio n„o se encontra no sistema!.\n");
+    	printf("O usu√°rio n√£o se encontra no sistema!.\n");
     	system("pause");
 	}
 }
 
 int main()
 {
-    int opcao = 0; //Definindo vari·veis
+    int opcao = 0; //Definindo vari√°veis
     int laco = 1;
 
     while (laco == 1)
     {
         system("cls");
-        setlocale(LC_ALL, "PortuguÍs"); //Definindo vari·veis
-        printf("### CartÛrio da EBAC ###\n\n"); //inicio do menu
-        printf("Escolha a opÁ„o desejada do menu\n\n");
+        setlocale(LC_ALL, "Portugu√™s"); //Definindo vari√°veis
+        printf("### Cart√≥rio da EBAC ###\n\n"); //inicio do menu
+        printf("Escolha a op√ß√£o desejada do menu\n\n");
         printf("\t1 - Nomes de registradores\n");
         printf("\t2 - Consultar nomes\n");
         printf("\t3 - Removendo nomes\n");
-        printf("OpÁ„o: "); //fim do menu
-        scanf("%d", &opcao); //armazenando a escolha do usu·rio
+	printf("\t4 - Sair do sistema\n\n");
+        printf("Op√ß√£o: "); //fim do menu
+        scanf("%d", &opcao); //armazenando a escolha do usu√°rio
         system("cls");
-        switch (opcao) //inicio da seleÁ„o
+        switch (opcao) //inicio da sele√ß√£o
         {
         case 1:
             registro();
@@ -134,11 +135,17 @@ int main()
         case 3:
             deletar();
             break;
+
+	case 4:
+		printf("Obrigada por utilizar o sistema!\n");
+		return 0;
+		break;
+		
         default:
-            printf("Essa opÁ„o n„o est· disponÌvel!\n");
+            printf("Essa op√ß√£o n√£o est√° dispon√≠vel!\n");
             system("pause");
             break;
-        } //fim da seleÁ„o
+        } //fim da sele√ß√£o
     }
 
 }
